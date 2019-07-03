08bfa940e47b4c7177170aca519644f77261fb48
Julian Parrish <parrishjulian62@gmail.com>
2019-07-03 11:06:30 -0400

    Model Test

--- spec/models/pull_request_comment_spec.rb
+++ spec/models/pull_request_comment_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe PullRequestComment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".set_data" do
+    it "sets pull request data" do
+      PullRequestComment.set_data("John", "Good job", "this_is_a_url_string")
+      comment = PullRequestComment.find_by_user("John")
+      expect(comment.body).to eq "Good job"
+    end
+  end
 end
+
